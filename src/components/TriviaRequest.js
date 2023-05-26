4d46a57d4f06bb08dbfe7fc20e8055d28682929c
agorbacheva-y <agorbacheva.y@gmail.com>
2023-05-26 13:52:34 +0200

    map thru array to show one question

--- src/components/TriviaRequest.js
+++ src/components/TriviaRequest.js
@@ -3,34 +3,56 @@ import he from "he";
 import Card from "react-bootstrap/Card";
 
 const TriviaRequest = () => {
+  // state for storing response from API (array of objects)
   const [ trivia, setTrivia ] = useState([]);
-  
+
+  // state for showing question text
+  const [ isShown, setIsShown ] = useState(false);
+
   const fetchTrivia = async () => {
     const response = await fetch("https://opentdb.com/api.php?amount=5&category=10&difficulty=medium&type=multiple");
     const data = await response.json();
-    
     setTrivia(data.results);
     
-    //console.log(trivia); // returns empty array, then several minutes later returns response
-    //console.log(data); // returns response immediately
+    console.log(trivia); // returns empty array, then several minutes later returns response
+    console.log(data); // returns response immediately
   };
 
-  // call function to see data
+  // call function to set data
   useEffect(() => {
     fetchTrivia();
   },[]);
 
-  let n = Math.floor(Math.random() * trivia.length);
+  let newQuestion = trivia.map(({question: value}) => value );
+  console.log(newQuestion);
+
+  let n = Math.floor(Math.random() * newQuestion.length);
   let text = "";
-  text = trivia[n]?.question; // ? checks if trivia array exists, then executes
-  // console.log(text);
+  text = newQuestion[n];
+  console.log(text);
+
+  // function to show card with question
+  const handleClick = (e) => {
+    e.preventDefault();
+    setIsShown(true);
+  };
 
   return (
     <>
       <h1>Trivia Quiz</h1>
       <Card body>
-        {text}
+        <button
+          onClick={handleClick}
+        >
+          Start
+        </button>
       </Card>
+
+      {isShown && (
+        <Card body>
+          {text}
+        </Card>
+      )}
     </>
   );
 };
@@ -45,4 +67,10 @@ export default TriviaRequest;
 ))}
 */
 
-// he.decode doesnt work now???
+  //text = trivia[n]?.question; // ? checks if trivia array exists, then executes
+
+  // let n = Math.floor(Math.random() * trivia.length);
+  // let text = "";
+  // text = trivia[n];
+
+  // console.log(text);
