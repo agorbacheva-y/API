cc43122b13792c657a78f378d6600b5ec1529658
agorbacheva-y <agorbacheva.y@gmail.com>
2023-06-07 10:50:50 +0200

    shuffle multiple choice

--- src/components/MultipleChoice.js
+++ src/components/MultipleChoice.js
@@ -1,25 +1,46 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import he from "he";
-//import Shuffle from "./Shuffle";
 
 const MultipleChoice = ({ triviaQues, index, checked, setChecked }) => {
+  // state for storing choices
+  const [ multiChoices, setMultiChoices ] = useState([]);
+
+  // state for current choices
+  const [ currentChoices, setCurrentChoices ] = useState([]);
+
+  // state for shuffled choices
+  const [ shuffledChoices, setShuffledChoices ] = useState([]);
+ 
   // array of letters for multiple choice
   const letters = ["A", "B", "C", "D"];
 
+  useEffect(() => {
+    setMultiChoices(triviaQues.map(({choices}) => choices));
+  },[triviaQues]);
+
+  useEffect(() => {
+    setCurrentChoices(multiChoices[index]);
+  },[multiChoices, index]);
+
+  useEffect(() => {
+    setShuffledChoices(currentChoices
+      .map(value => ({ value, sort: Math.random() }))
+      .sort(( a, b ) => a.sort - b.sort)
+      .map(({ value }) => value)
+      )
+  },[currentChoices]);
+
+  //console.log(currentChoices);
+  //console.log(shuffledChoices);
+
   const handleClick = (e) => {
     setChecked(e.target.value);
-    
   };
   console.log(checked);
 
-  // useEffect(() => {
-  //   Shuffle(triviaQues.choices);
-  // },[]);
-
-
   return (
     <div>
-      {triviaQues[index].choices?.map((item, index) => 
+      {shuffledChoices.map((item, index) => 
         (
           <div key={index}>
             <button 
@@ -41,3 +62,12 @@ const MultipleChoice = ({ triviaQues, index, checked, setChecked }) => {
 export default MultipleChoice;
 
 // put choices in array then shuffle
+
+// fisher-yates shuffle
+// const shuffle = (multiChoice) => {
+//   for (var i = multiChoice.length - 1; i > 0; i--) {
+//     var j = Math.floor(Math.random() * (i + 1));
+//     [multiChoice[i], multiChoice[j]] = [multiChoice[j], multiChoice[i]];
+//   }
+//   return multiChoice;
+// };
